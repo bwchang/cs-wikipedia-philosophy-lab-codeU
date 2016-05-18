44fb3313c382071afae21e12101e1faecba26e93
Betty Chang <bwchang@berkeley.edu>
2016-05-18 12:34:14 -0700

    Done.

--- javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
+++ javacs-lab05/src/com/flatironschool/javacs/WikiPhilosophy.java
@@ -13,6 +13,9 @@ import org.jsoup.select.Elements;
 public class WikiPhilosophy {
 	
 	final static WikiFetcher wf = new WikiFetcher();
+	private static List<String> visited = new ArrayList<String>();
+	private static String currentUrl;
+	private static int valid = 1;
 	
 	/**
 	 * Tests a conjecture about Wikipedia and Philosophy.
@@ -29,23 +32,91 @@ public class WikiPhilosophy {
 	 */
 	public static void main(String[] args) throws IOException {
 		
-        // some example code to get you started
-
-		String url = "https://en.wikipedia.org/wiki/Java_(programming_language)";
+        String url = "https://en.wikipedia.org/wiki/Greek_language";
 		Elements paragraphs = wf.fetchWikipedia(url);
-
-		Element firstPara = paragraphs.get(0);
 		
-		Iterable<Node> iter = new WikiNodeIterable(firstPara);
-		for (Node node: iter) {
-			if (node instanceof TextNode) {
-				System.out.print(node);
-			}
-        }
+		int counter = 0;
+		currentUrl = url;
 
-        // the following throws an exception so the test fails
-        // until you update the code
-        String msg = "Complete this lab by adding your code and removing this statement.";
-        throw new UnsupportedOperationException(msg);
+        String nextPage = processPage(paragraphs);
+        while (!nextPage.equals("https://en.wikipedia.org/wiki/Philosophy")) {
+        	if (nextPage == null) {
+        		exit(0);
+        		return;
+        	}
+        	visited.add(nextPage);
+        	currentUrl = nextPage;
+        	Elements next = wf.fetchWikipedia(nextPage);
+        	nextPage = processPage(next);
+        }
+        visited.add(nextPage);
+        exit(1);
+
+	}
+
+	private static int countMatches(String text, String s) {
+		int count = 0;
+		int i = 0;
+		while ((i = text.indexOf(s, i)) != -1) {
+		    i++;
+		    count++;
+		}
+		return count;
+	}
+
+	private static String processPage(Elements paragraphs) {
+		for (Element paragraph : paragraphs) {
+			int parentheses = 0;
+			Iterable<Node> iter = new WikiNodeIterable(paragraph);
+			for (Node node: iter) {
+				int open = 0, close = 0;
+				String nodeName = node.nodeName();
+				if (node instanceof TextNode) {
+					open = countMatches(node.toString(), "(");
+					close = countMatches(node.toString(), ")");
+					parentheses += open - close;
+				}
+				if (nodeName.equals("a")) {
+					String linkUrl = node.attr("abs:href");
+					if (visited.contains(linkUrl)) {
+						return null;
+					}
+					if (linkUrl.startsWith(currentUrl) || !linkUrl.startsWith("https://en.wikipedia.org") || parentheses != 0) {
+						continue;
+					} else {
+						Element nodeElement = (Element) node;
+						Elements parents = nodeElement.parents();
+						for (Element parent : parents) {
+							if (parent.tagName().equals("i") || parent.tagName().equals("em")) {
+								valid = 0;
+								break;
+							}
+						}
+						if (valid != 0) {
+							return linkUrl;
+						} else {
+							valid = 1;
+						}
+					}
+				}
+	        }
+		}
+		return null;
+	}
+
+	private static void print(List<String> urls) {
+		System.out.println("Visited pages:");
+		for (String s : urls) {
+			System.out.println(s);
+		}
+	}
+
+	private static void exit(int success) {
+		if (success == 0) {
+			System.out.println("Failure!");
+		} else {
+			System.out.println("Success!");
+		}
+		print(visited);
 	}
 }
